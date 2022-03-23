163a7393547188ca3a46965991ea1725cfb7900e
jinyess <jine0208@kakao.com>
2022-03-23 22:37:00 +0900

    flask test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 import re
 import urllib.request
@@ -10,8 +10,18 @@ osulloc_url = 'https://www.osulloc.com/kr/ko/shop/item/list?1=1&sort=review'
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    html = urllib.request.urlopen(osulloc_url).read()
-    soup = BeautifulSoup(html, 'html.parser')
-    sites = soup.find_all('div', {'class':'item'})
-    return 'dddd'
+def index():
+    return render_template('index.html')
+# @app.route('/', method=['POST'])
+# def index_post():
+#     html = urllib.request.urlopen(osulloc_url).read()
+#     soup = BeautifulSoup(html, 'html.parser')
+#     sites = soup.find_all('div', {'class': 'item'})
+#     link = soup.select_one('#pagination > a:last-child')['href']
+#     totalpage = int(link.split('p=')[1])
+#     return 'dddd'
+
+if __name__ == "__main__":
+    # app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.debug = True
+    app.run()
